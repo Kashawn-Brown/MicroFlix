4afb6d5dfddc495c81e3968933299633671b4512
Kashawn-Brown <kashawnbrown@hotmail.com>
2025-12-01 16:40:55 -0500

    test: configure H2 profile and fix service integration tests

--- modules/user-service/src/test/java/com/microflix/userservice/UserServiceApplicationTests.java
+++ modules/user-service/src/test/java/com/microflix/userservice/UserServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.microflix.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceApplicationTests {
 
 	@Test
